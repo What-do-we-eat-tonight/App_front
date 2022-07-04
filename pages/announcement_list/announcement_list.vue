b29b0a5fafbf11918c6351d96618232bb6c40bbb
quanchenliu <108453067+quanchenliu@users.noreply.github.com>
2022-07-04 16:16:23 +0800

    Add files via upload

--- pages/announcement_list/announcement_list.vue
+++ pages/announcement_list/announcement_list.vue
@@ -1,23 +1,29 @@
-<template>
+<template><!-- 课程的公告栏 -->
 	<view class="index">
 		<!-- u-navbar 自定义导航栏内容 -->
 		<!-- 在pages.json文件中修改对应的style -->
-		<!-- 课程的公告栏 -->
+		<!-- <u-badge :is-dot="true" type="error" :offset="[46,80]"></u-badge> -->
 		<u-navbar back-text="返回" title="课程公告" @click="toback" index="back"></u-navbar>
 		
-		<view v-for="(item,idx) in announcement_list" :key="idx" class="announcement">
-			<view class="content_title">公告内容为：</view>
-			<view class="content">{{item.content}}</view>
-			<u-row gutter="66">//这部分可以改得更好看
-				<u-col span="66">
-					<view class="teacher_no">教师编号：{{item.tno}}</view>
-				</u-col>
-				<u-col span="66">
-					<view class="class_no">课程编号：{{item.cno}}</view>
-				</u-col>
-			</u-row>
-			<u-button class="getclick">确认收到</u-button>
+		<view v-for="(item,idx) in announcement_list" :key="idx" >
+			<!-- 点击进入课程公告内容页面 -->
+			<uni-collapse :accordion="true" :title="get_title(item.content)">
+				<!-- 课程公告标题栏 -->
+				<uni-collapse-item  align="left">
+					<!-- 新消息提示标志 -->
+					<u-badge :is-dot="true" type="error" :offset="[46,80]"></u-badge>
+					<!-- 课程公告内容栏 -->
+					<u-cell-item  align="left" class="content">
+						<view class="content">{{item.content}}</view>
+						{{item.submit_time}}
+						<!-- 消息提示模块 -->
+						<u-badge :is-dot="true" type="error" :offset="[46,80]"></u-badge>
+					</u-cell-item>
+				</uni-collapse-item>
+				
+			</uni-collapse>
 		</view>
+		
 	</view>
 </template>
 
@@ -25,11 +31,24 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello',
+				isAnnouncement: false,
 				c:{
-					cno:'',
+					cno:'',//该课程的课程好
 				},
-				announcement_list:[],
+				announcement_list:[
+					// {
+					//     "ano": "1",
+					// 	"atitle":"课程简述",
+					//     "tname": "可望",
+					//     "cno": "COMP1221",
+					//     "cname": "计算机体系结构",
+					//     "content": "本课程作为计算机专业核心课程，介绍形式语言、自动机、文法、词法分析、语法分析和语义分析，既有独立的II型和III型语言理论，也有强调原理性的程序分析和转换方法。通过本课程学习，认识一类计算的规律，掌握通用的理论和方法。",
+					//     "submit_time": "2022-06-30 10:00:14",
+					// 	"read":"true"
+					// }
+					],//课程公告列表内容
+				title_x:'',
+				title:[{}]
 			}
 		},
 		//生命周期方法，表示页面正在加载还未显示之前执行的逻辑。
@@ -37,19 +56,28 @@
 			//this.getMealList();
 		},
 		methods: {
+			//获取课程公告列表
 			async test(){
-				//this.c.cno = uni.getStorageSync("login_id");
+				//获取课程号
 				this.c.cno = uni.getStorageSync("cno");
-				console.log(this.c);
+				//获取课程公告列表
 				this.announcement_list = await this.$u.post('/student_user/announcement-list',this.c);
 			},
+			
+			async get_title(content){
+				var t = content;
+				var title = t.split('，');
+				console.log(title[0]);
+				return title[0];
+			},
+			//返回课程列表页面
 			async toback(back){
 				console.log('========', back);
 				this.$u.route({
 					url: '/pages/class/class',//修改成“公告”页面地址
 					type: 'to'
 				});
-			}
+			},
 		},
 		onShow(){
 			this.test();
@@ -63,42 +91,9 @@
 		width: 600rpx;
 		height: 500rpx;
 		margin: auto;
+		text-align: center;
 	}
-	.announcement{
-		margin: 60rpx auto auto;
-		width: 100%;
-		height: auto;
-		background-color: white;
-	}
-	
-	.content_title{
-		margin: 10rpx auto;
-		width:90%;
-		font-family:Helvetica;
-		font-size: 1.2rem;
-	}
-	
 	.content{
-		margin: 10rpx auto;
-		width:90%;
-		font-family:Helvetica;
-		font-size: 1.2rem;
-		color: red;
-	}
-	.teacher_no{
-		margin: 10rpx auto 0rpx;
-		width:100%;
-		height:40rpx;
-	}
-	.class_no{
-		margin: 10rpx auto;
-		width:100%;
-		height:40rpx;
-	}
-	.getclick{
-		margin: 10rpx auto;
-		width:30%;
-		height:40rpx;
+		text-align: left;
 	}
 </style>
-
